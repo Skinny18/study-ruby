58919eb189e12a068f39a5630fc7d1c8f5529ed6
Gabriel Carlos <gcgabriel257@gmail.com>
2023-05-24 00:02:49 -0400

    firt commit: ruby

---
+++ exemplo01.rb
@@ -0,0 +1,20 @@
+puts "Digite seu nome:"
+nome = gets.chomp
+ 
+puts "Ola " + nome
+
+puts "++++++++++++++"
+
+puts "Com o inspect >> " + nome.inspect
+
+puts "Digite um numero:"
+a = gets.to_i
+
+puts "Digite outro numero:"
+b = gets.to_f
+
+puts "to_i:"
+puts a
+puts "---------------"
+puts "to_f"
+puts b
